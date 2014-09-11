35484ba68b8c5e527ec1a98e07de492a2e6dffb3
carloscrespog <carlos.crespog@gmail.com>
2014-09-11 08:42:02 +0200

    first commit

---
+++ build/CMakeFiles/3DSelfie.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/3DSelfie.dir/3DSelfie.cpp.o"
+  "3DSelfie.pdb"
+  "3DSelfie"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/3DSelfie.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
