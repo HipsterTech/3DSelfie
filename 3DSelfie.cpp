99bf39bffcba081718e849d2a7204f4d37e4dc6e
Carlos Crespo <carlos.crespog@gmail.com>
2015-08-08 18:48:20 +0200

    Acquisition and visualization with PCLVisualizer, camera parameters on screen

--- 3DSelfie.cpp
+++ 3DSelfie.cpp
@@ -1,156 +1,89 @@
 // Original code by Geoffrey Biggs, taken from the PCL tutorial in
 // http://pointclouds.org/documentation/tutorials/pcl_visualizer.php
 
-// Simple Kinect viewer that also allows to write the current scene to a .pcd
-// when pressing SPACE.
-
 #include <iostream>
+#include <mutex>
 
 #include <pcl/io/openni_grabber.h>
 #include <pcl/io/pcd_io.h>
-#include <pcl/visualization/cloud_viewer.h>
-#include <pcl/console/parse.h>
+#include <pcl/visualization/pcl_visualizer.h>
 
-using namespace std;
-using namespace pcl;
+using std::cout;
+using std::endl;
 
-PointCloud<PointXYZRGBA>::Ptr cloudptr(new PointCloud<PointXYZRGBA>);
-PointCloud<PointXYZ>::Ptr fallbackCloud(new PointCloud<PointXYZ>);
-//boost::shared_ptr<visualization::CloudViewer> viewer (new visualization::CloudViewer("v"));
-Grabber* kinectGrabber;
-unsigned int filesSaved = 0;
-bool saveCloud(false), noColour(false);
 
+static pcl::Grabber* kinectGrabber;
+static boost::shared_ptr<pcl::visualization::PCLVisualizer> _viewer;
+static std::mutex _mtx;  
+std::vector<pcl::visualization::Camera> cam; 
+
+//Called every time there's a new frame 
 void
-printUsage(const char* programName)
-{
-    cout << "Usage: " << programName << " [options]"
-         << endl
-         << endl
-         << "Options:\n"
-         << endl
-         << "\t<none>     start capturing from a Kinect device.\n"
-         << "\t-v NAME    visualize the given .pcd file.\n"
-         << "\t-h         shows this help.\n";
-}
+grabber_callback(const pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr& cloud)
+{        
+    static bool first = true;
 
-void
-grabberCallback(const PointCloud<PointXYZRGBA>::ConstPtr& cloud)
-{
-    // if (! viewer->wasStopped())
-    //     viewer->showCloud(cloud);
-        
-    if (saveCloud)
+    if(first)
     {
-        stringstream stream;
-        stream << "inputCloud" << filesSaved << ".pcd";
-        string filename = stream.str();
-        if (io::savePCDFile(filename, *cloud, true) == 0)
-        {
-            filesSaved++;
-            cout << "Saved " << filename << "." << endl;
-        }
-        else PCL_ERROR("Problem saving %s.\n", filename.c_str());
-        
-        saveCloud = false;
+        _mtx.lock();
+        _viewer->addPointCloud<pcl::PointXYZRGBA>(cloud, "samplecloud");
+        _mtx.unlock();
+        //_viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud");
+        first = false;
+        return;
     }
+    else
+    {
+        _mtx.lock();
+        _viewer->updatePointCloud(cloud,"samplecloud");
+        _viewer->getCameras(cam); 
+        _mtx.unlock();    
+    }
+
+    cout << "Cam: " << endl 
+             << " - pos: (" << cam[0].pos[0] << ", "    << cam[0].pos[1] << ", "    << cam[0].pos[2] << ")" << endl 
+             << " - view: ("    << cam[0].view[0] << ", "   << cam[0].view[1] << ", "   << cam[0].view[2] << ")"    << endl 
+             << " - focal: ("   << cam[0].focal[0] << ", "  << cam[0].focal[1] << ", "  << cam[0].focal[2] << ")"   << endl;
+     
+
 }
 
-void
-keyboardEventOccurred(const visualization::KeyboardEvent& event,
-    void* nothing)
+//Creates and configures the viewer, returning a shared pointer to it
+boost::shared_ptr<pcl::visualization::PCLVisualizer>
+create_viewer()
 {
-    if (event.getKeySym() == "space" && event.keyDown())
-        saveCloud = true;
-}
+    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+    viewer->setBackgroundColor (0, 0, 0);
+    viewer->addCoordinateSystem (1.0);
+    viewer->initCameraParameters ();
+    viewer->setCameraPosition(0,0,-1,0,-1,0,0,0,1); 
+    viewer->getCameras(cam); 
+    return (viewer);    
+} 
 
-boost::shared_ptr<visualization::CloudViewer>
-createViewer()
-{
-    boost::shared_ptr<visualization::CloudViewer> v
-        (new visualization::CloudViewer("3D Viewer"));
-    v->registerKeyboardCallback(keyboardEventOccurred);
-    
-    return(v);
-}
 
 int
 main(int argc, char** argv)
 {
-    if (console::find_argument(argc, argv, "-h") >= 0)
-    {
-        printUsage(argv[0]);
-        return 0;
-    }
+
+    //Set up viewer
+    _viewer = create_viewer();
     
-    bool justVisualize(false);
-    string filename;
-    if (console::find_argument(argc, argv, "-v") >= 0)
+    //Set up the grabber and register the callback function
+    kinectGrabber = new pcl::OpenNIGrabber();
+    if (kinectGrabber == 0) return false;
+    boost::function<void (const pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr&)> f = boost::bind(&grabber_callback, _1);
+    kinectGrabber->registerCallback(f);
+
+    //Start the loop
+    kinectGrabber->start();
+    while (!_viewer->wasStopped ())
     {
-        if (argc != 3)
-        {
-            printUsage(argv[0]);
-            return 0;
-        }
+        _mtx.lock();
+        _viewer->spinOnce (100);
+        _mtx.unlock();
+        boost::this_thread::sleep (boost::posix_time::microseconds (100));
+    }
         
-        filename = argv[2];
-        justVisualize = true;
-    }
-    else if (argc != 1)
-    {
-        printUsage(argv[0]);
-        return 0;
-    }
-    
-    // if (justVisualize)
-    // {
-    //     try
-    //     {
-    //         io::loadPCDFile<PointXYZRGBA>(filename.c_str(), *cloudptr);
-    //     }
-    //     catch (PCLException e1)
-    //     {
-    //         try
-    //         {
-    //             io::loadPCDFile<PointXYZ>(filename.c_str(), *fallbackCloud);
-    //         }
-    //         catch (PCLException e2)
-    //         {
-    //             return -1;
-    //         }
-            
-    //         noColour = true;
-    //     }
-        
-    //     cout << "Loaded " << filename << "." << endl;
-    //     if (noColour)
-    //         cout << "This file has no RGBA colour information present." << endl;
-    // }
-    // else
-    // {
-        kinectGrabber = new OpenNIGrabber();
-        if (kinectGrabber == 0)
-            return false;
-        boost::function<void (const PointCloud<PointXYZRGBA>::ConstPtr&)> f =
-            boost::bind(&grabberCallback, _1);
-        kinectGrabber->registerCallback(f);
-    //}
-    
-    //viewer = createViewer();
-    
-    // if (justVisualize)
-    // {
-    //     if (noColour)
-    //         viewer->showCloud(fallbackCloud);
-    //     else viewer->showCloud(cloudptr);
-    // }
-    // else 
-        saveCloud = true;
-        kinectGrabber->start();
-    
-    while (true)
-        boost::this_thread::sleep(boost::posix_time::seconds(1));
-    
-    if (! justVisualize)
-        kinectGrabber->stop();
+    kinectGrabber->stop();
 }
